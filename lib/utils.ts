df9a45c57554c6556af1a3d782b1cfe99b8e299e
chaojiang <chaojiangcn@outlook.com>
2024-12-21 17:37:15 +0800

    🐞 fix:修改时间显示

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,13 +8,25 @@ export function cn(...inputs: ClassValue[]) {
 export function timeAgo(time: string | number | Date): string {
   const now = new Date();
   const past = new Date(time);
-  const diff = now.getTime() - past.getTime();
 
+  const years = now.getFullYear() - past.getFullYear();
+  const months = now.getMonth() - past.getMonth();
+  const days = now.getDate() - past.getDate();
+
+  let totalMonths = years * 12 + months;
+
+  // 如果当前日期的天数小于过去的日期的天数，则减去一个月
+  if (days < 0) {
+    totalMonths -= 1;
+  }
+  if (totalMonths > 0) {
+    return totalMonths === 1 ? '1个月前' : `${totalMonths}个月前`;
+  }
+  
+  const diff = now.getTime() - past.getTime();
   const seconds = Math.floor(diff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const years = Math.floor(days / 365);
 
   if (years > 0) {
     return years === 1 ? '一年前' : `${years}年前`;
